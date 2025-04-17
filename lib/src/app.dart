e8816fed34d3f9b386dbd4a61c32ad1992a674b0
David Sent <david.sent@app-akademie.com>
2025-04-17 09:58:14 +0200

    Refactor theme configuration in MaterialApp to use ThemeData.from with ColorScheme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,9 +7,24 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.light,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(
+            brightness: Brightness.light, seedColor: Colors.blue),
+      ).copyWith(
+        cardTheme: CardThemeData(
+          color: Colors.grey,
+          elevation: 16,
+        ),
+        filledButtonTheme: FilledButtonThemeData(
+          style: FilledButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 75, 95, 128),
+          ),
+        ),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+        ),
+      ),
       home: MainScreen(),
     );
   }
