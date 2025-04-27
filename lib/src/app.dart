dea6a00e57c217ec5b51fd38c880a1827bd0ad42
David Sent <david.sent@app-akademie.com>
2025-04-27 10:46:45 +0200

    Refactor MainScreen layout and remove redundant theme configuration in App

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,23 +7,6 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.from(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-      ).copyWith(
-        cardTheme: CardThemeData(
-          color: Colors.grey,
-          elevation: 16,
-        ),
-        filledButtonTheme: FilledButtonThemeData(
-          style: FilledButton.styleFrom(
-            backgroundColor: const Color.fromARGB(255, 75, 95, 128),
-          ),
-        ),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Colors.black,
-          foregroundColor: Colors.white,
-        ),
-      ),
       home: MainScreen(),
     );
   }
--- lib/src/main_screen.dart
+++ lib/src/main_screen.dart
@@ -10,35 +10,37 @@ class MainScreen extends StatelessWidget {
         onPressed: () {},
         child: Icon(Icons.person),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Card(
-          child: Column(
-            spacing: 32,
-            children: [
-              Slider(
-                value: 0.8,
-                onChanged: (newValue) {},
-              ),
-              Slider(
-                value: 0.2,
-                onChanged: (newValue) {},
-              ),
-              Switch(
-                value: true,
-                onChanged: (newValue) {},
-              ),
-              Switch(value: false, onChanged: (newValue) {}),
-              ElevatedButton(onPressed: () {}, child: Text("Elevated")),
-              FilledButton(onPressed: () {}, child: Text("Filled")),
-              OutlinedButton(onPressed: () {}, child: Text("Outlined")),
-              CircularProgressIndicator(),
-              LinearProgressIndicator(),
-              TextField(),
-              FilledButton(onPressed: () {}, child: Text("Filled")),
-              FilledButton(onPressed: () {}, child: Text("Filled")),
-              FilledButton(onPressed: () {}, child: Text("Filled")),
-            ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Card(
+            child: Column(
+              spacing: 32,
+              children: [
+                Slider(
+                  value: 0.8,
+                  onChanged: (newValue) {},
+                ),
+                Slider(
+                  value: 0.2,
+                  onChanged: (newValue) {},
+                ),
+                Switch(
+                  value: true,
+                  onChanged: (newValue) {},
+                ),
+                Switch(value: false, onChanged: (newValue) {}),
+                ElevatedButton(onPressed: () {}, child: Text("Elevated")),
+                FilledButton(onPressed: () {}, child: Text("Filled")),
+                OutlinedButton(onPressed: () {}, child: Text("Outlined")),
+                CircularProgressIndicator(),
+                LinearProgressIndicator(),
+                TextField(),
+                FilledButton(onPressed: () {}, child: Text("Filled")),
+                FilledButton(onPressed: () {}, child: Text("Filled")),
+                FilledButton(onPressed: () {}, child: Text("Filled")),
+              ],
+            ),
           ),
         ),
       ),
